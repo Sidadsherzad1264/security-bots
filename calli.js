c8f17be31c11d4b635a7e485a83f1b023ab79cac
Sidadsherzad1264 <105194122+Sidadsherzad1264@users.noreply.github.com>
2022-05-09 09:30:40 +0300

    Update calli.js

--- calli.js
+++ calli.js
@@ -37,7 +37,7 @@ const dateFormat = require("dateformat");
 var table = require("table").table;
 const Discord = require("discord.js");
 const cmd = require("node-cmd");
-const prefix = "s!";
+const prefix = "z!";
 const cooldown = new Set();
 const cdtime = 5;
 ///////////////////////////////////////////////////////////////////////////////
@@ -66,7 +66,7 @@ calli.on("message", message => {
   }
 });
 ///////////////////////////////////////////////////////////////////////////////
-calli.login("")
+calli.login("OTczMTA2MzExODQ1NzkzODAy.GrfcoW.KKPdY0SFsObasAiC1YjcY525nwHIjTU0eqdlek")
 ///////////////////////////////////////////////////////////////////////////////
 const callienabled  = "";
 const callidisabled = "";
